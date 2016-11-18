12acb8da8c7a1e0740baeee6958fe6450c2b50e7
NicolaiLouise <nicolaicaasi@gmail.com>
2016-11-18 14:23:41 +0800

    edited News.php

--- Exercise6/codeigniter/application/controllers/News.php
+++ Exercise6/codeigniter/application/controllers/News.php
@@ -2,7 +2,9 @@
 class News extends CI_Controller {
  
     public function __construct()
-    {
+    {   
+	
+	
         parent::__construct();
         $this->load->model('news_model');
         $this->load->helper('url_helper');
