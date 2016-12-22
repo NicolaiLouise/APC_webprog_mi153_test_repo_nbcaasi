49b6142c85c7a1513ff30d92df0d4e857aa5bd59
NicolaiLouise <nicolaicaasi@gmai.com>
2016-12-22 13:12:20 +0800

    updated edit.php

--- Exercise6/codeigniter/application/views/news/edit.php
+++ Exercise6/codeigniter/application/views/news/edit.php
@@ -5,8 +5,10 @@
 <?php echo form_open('news/edit/'.$news_item['id']); ?>
     <table>
         <tr>
+		
             <td><label for="title">Title</label></td>
             <td><input type="input" name="title" size="50" value="<?php echo $news_item['title'] ?>" /></td>
+			
         </tr>
         <tr>
             <td><label for="text">Text</label></td>
