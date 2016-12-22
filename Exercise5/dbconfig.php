8e2a220a227aba5452b66a9c4247e63ed542513b
NicolaiLouise <nicolaicaasi@gmai.com>
2016-12-22 14:06:16 +0800

    updated dbconfig.php

--- Exercise5/dbconfig.php
+++ Exercise5/dbconfig.php
@@ -5,7 +5,7 @@ $host = "localhost";
 $user = "root"; 
 $password = ""; 
 $datbase = "dbtuts"; 
-$con = mysqli_connect($host,$user,$password);
-mysqli_select_db($con,$datbase);
+$con = mysqli_connect($host,$user,$password); 
+mysqli_select_db($con,$datbase); 
 
 ?>
