ba56a7b942a90d50be2a7350d4749b37b7c8820a
NicolaiLouise <nicolaicaasi@gmai.com>
2016-12-22 13:31:16 +0800

    updated header.php

--- Exercise6/codeigniter/application/views/templates/header.php
+++ Exercise6/codeigniter/application/views/templates/header.php
@@ -3,6 +3,6 @@
 		<title> CodeIgniter Tutorial </title>
 	</head>
 	<body>
-		<h1>Simple CRUD</h1>
+		<h1> Simple CRUD </h1>
 		<p><a href="<?php echo site_url('news'); ?>">Home</a> | <a href="<?php echo site_url('news/create'); ?>">Add News</a></p>
 		</body>
