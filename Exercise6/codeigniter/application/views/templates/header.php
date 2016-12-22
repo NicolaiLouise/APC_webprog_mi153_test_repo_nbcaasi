ed353987af62801097bcc0dc9fbb8fdc32e12b03
NicolaiLouise <nicolaicaasi@gmai.com>
2016-12-22 13:43:07 +0800

    updated header.php

--- Exercise6/codeigniter/application/views/templates/header.php
+++ Exercise6/codeigniter/application/views/templates/header.php
@@ -7,7 +7,6 @@
 	</head>
 	
 	<body>
-	
 		<h1> Simple CRUD </h1>
 		<p><a href="<?php echo site_url('news'); ?>">Home</a> | <a href="<?php echo site_url('news/create'); ?>">Add News</a></p>
 		</body>
