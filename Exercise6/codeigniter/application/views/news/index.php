59d17510b4569079796e096066b5be7719329cf3
NicolaiLouise <nicolaicaasi@gmai.com>
2016-12-22 13:08:48 +0800

    updated index.php

--- Exercise6/codeigniter/application/views/news/index.php
+++ Exercise6/codeigniter/application/views/news/index.php
@@ -11,6 +11,7 @@
             <td><?php echo $news_item['title']; ?></td>
             <td><?php echo $news_item['text']; ?></td>
             <td>
+			
                 <a href="<?php echo site_url('news/'.$news_item['slug']); ?>">View</a> | 
                 <a href="<?php echo site_url('news/edit/'.$news_item['id']); ?>">Edit</a> | 
                 <a href="<?php echo site_url('news/delete/'.$news_item['id']); ?>" onClick="return confirm('Are you sure you want to delete?')">Delete</a>
