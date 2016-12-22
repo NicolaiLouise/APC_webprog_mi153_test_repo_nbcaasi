0ebf528ab05284de1209f0301f291db1b5391b66
NicolaiLouise <nicolaicaasi@gmai.com>
2016-12-22 14:20:48 +0800

    updated index.php

--- Exercise5/index.php
+++ Exercise5/index.php
@@ -3,7 +3,7 @@ include_once 'dbconfig.php';
 
 
 // delete condition
-if(isset($_GET['delete_id']))
+if(isset($_GET['delete_id']))  
 {
  $sql_query="DELETE FROM users WHERE user_id=".$_GET['delete_id'];
  mysqli_query($con, $sql_query);
